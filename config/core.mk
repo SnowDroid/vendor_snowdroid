0a0487a10dc140835857434a7657d52dfaa8d286
jbott <snowleopardjb@gmail.com>
2013-11-22 18:10:25 -0500

    Add init.rom.rc

--- config/core.mk
+++ config/core.mk
@@ -2,3 +2,7 @@
 
 SUPERUSER_EMBEDDED := true
 SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
+
+# Copy init.rom.rc into place
+PRODUCT_COPY_FILES += \
+    vendor/jbott/rootdir/init.rom.rc:root/init.rom.rc
