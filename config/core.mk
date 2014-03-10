c07fccaf70cdb65f379359be4973ef79f14891d3
jbott <snowleopardjb@gmail.com>
2014-03-09 20:40:26 -0400

    Add superuser to product packages

--- config/core.mk
+++ config/core.mk
@@ -6,3 +6,8 @@ SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
 # Copy init.rom.rc into place
 PRODUCT_COPY_FILES += \
     vendor/SnowDroid/rootdir/init.rom.rc:root/init.rom.rc
+
+# Build Superuser
+PRODUCT_PACKAGES += \
+    Superuser\
+    su
